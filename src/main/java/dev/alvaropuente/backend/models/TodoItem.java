699d71253ca93dee3fc382c0cbc808df54e329b3
Alvaro Puente <alvaropuentecorrea@gmail.com>
2023-08-19 23:16:05 +0200

    test DB postgres deploy

--- src/main/java/dev/alvaropuente/backend/models/TodoItem.java
+++ src/main/java/dev/alvaropuente/backend/models/TodoItem.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "todo_items")
+@Table(name = "todo_items", schema = "todo")
 public class TodoItem implements Serializable {
 
     @Id
