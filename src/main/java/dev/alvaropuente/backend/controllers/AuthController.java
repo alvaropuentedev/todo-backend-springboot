5668590b440a1b90b54768be1e38c05498176193
Alvaro Puente <alvaropuentecorrea@gmail.com>
2023-11-18 18:50:14 +0100

    Added cors

--- src/main/java/dev/alvaropuente/backend/controllers/AuthController.java
+++ src/main/java/dev/alvaropuente/backend/controllers/AuthController.java
@@ -6,12 +6,14 @@ import dev.alvaropuente.backend.auth.LoginRequest;
 import dev.alvaropuente.backend.auth.RegisterRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
