551528e47c4a4729e8ed1506ba023e53baa863d2
Alvaro Puente <alvaropuentecorrea@gmail.com>
2023-12-09 03:09:16 +0100

    Added @ManyToOne @OneToMany

--- src/main/java/dev/alvaropuente/backend/models/TodoItem.java
+++ src/main/java/dev/alvaropuente/backend/models/TodoItem.java
@@ -23,6 +23,10 @@ public class TodoItem implements Serializable {
     private String description;
 //    private LocalDateTime createdAt;
 //    private LocalDateTime updatedAt;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
 
 
--- src/main/java/dev/alvaropuente/backend/models/User.java
+++ src/main/java/dev/alvaropuente/backend/models/User.java
@@ -34,6 +34,9 @@ public class User implements UserDetails {
 
     @Enumerated(EnumType.STRING)
     Role role;
+    
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<TodoItem> todoItems;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
